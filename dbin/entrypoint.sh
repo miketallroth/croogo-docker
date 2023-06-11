f34de6538ee589b069d142f300a671c5448780ea
Mike Tallroth <mike.tallroth@gmail.com>
2023-06-10 19:59:33 -0500

    helper scripts unnecessary in current docker

--- dbin/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# runs when the container is started
-
-# developer convenience
-echo 'alias lsf="ls -F"' >> ~/.bashrc
-echo 'set -o vi' >> ~/.bashrc
-
-# run apache in foreground to keep container running
-exec apache2-foreground "$@"
